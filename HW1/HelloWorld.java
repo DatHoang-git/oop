f31c4d7c4ff4081e289691bf1bf4ca01e40d927d
Dathoang-git <datdohoang433@gmail.com>
2025-10-01 08:39:25 +0700

    Add Introduction.java : Gioi thieu sinh vien

--- HW1/HelloWorld.java
+++ HW1/HelloWorld.java
@@ -1,7 +1,8 @@
+package HW1;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-
         // Prints "Hello, World" in the terminal window.
         System.out.println("Hello, World");
     }
---
+++ HW1/Introduction.java
@@ -0,0 +1,37 @@
+package HW1;
+
+public class Introduction {
+
+    public static void main(String[] args) {
+        System.out.println("Do Hoang Dat" + "\t" + "24020060" + "\t" + "K69I-IT6" + "\t" + "DatHoang-git" + "\t" + "datdohoang433@gmail.com" + "\n");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.\n");
+        System.out.println("Take one down, pass it around,\n");
+
+        System.out.println("8 bottles of beer on the wall, 8 bottles of beer.\n");
+        System.out.println("Take one down, pass it around,\n");
+
+        System.out.println("7 bottles of beer on the wall, 7 bottles of beer.\n");
+        System.out.println("Take one down, pass it around,\n");
+
+        System.out.println("6 bottles of beer on the wall, 6 bottles of beer.\n");
+        System.out.println("Take one down, pass it around,\n");
+
+        System.out.println("5 bottles of beer on the wall, 5 bottles of beer.\n");
+        System.out.println("Take one down, pass it around,\n");
+
+        System.out.println("4 bottles of beer on the wall, 4 bottles of beer.\n");
+        System.out.println("Take one down, pass it around,\n");
+
+        System.out.println("3 bottles of beer on the wall, 3 bottles of beer.\n");
+        System.out.println("Take one down, pass it around,\n");
+
+        System.out.println("2 bottles of beer on the wall, 2 bottles of beer.\n");
+        System.out.println("Take one down, pass it around,\n");
+
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.\n");
+        System.out.println("Take one down, pass it around,\n");
+
+        System.out.println("No more bottles of beer on the wall.");
+    }
+
+}
